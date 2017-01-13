be34f2e7c9f4e8b68f032317963e7a77b5572abb
Lior Hirschfeld <Lior Hirschfeld>
2017-01-13 12:31:08 -0500

    Added Variable class

--- Variable.py
+++ Variable.py
@@ -1,1 +1,7 @@
-
+class Variable():
+    def __init__(self, val):
+        self.val = val
+    def __add__(self, other):
+        return Variable(self.val + other.val)
+    def __radd__(self, other):
+        return Variable(self.val + other.val)
